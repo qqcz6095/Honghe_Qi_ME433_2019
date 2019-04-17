3492225ef120070606119d35ea7388ef852c0099
qqcz6095 <qihhhanks@gmail.com>
2019-04-17 15:57:26 -0500

    I2C light on

--- HW #1.X/HW1 Code.c
+++ HW #1.X/HW1 Code.c
@@ -60,7 +60,7 @@ int main() {
     while(1) {
 	// use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 	// remember the core timer runs at half the sysclk
-        if (_CP0_GET_COUNT()>12000)
+        if (_CP0_GET_COUNT()>12000000)
             {
                 LATAbits.LATA4 =!LATAbits.LATA4;
                 _CP0_SET_COUNT(0);
---
+++ HW#5/HW_5.X/build/default/production/HW_5_I2C.o.d
@@ -0,0 +1,2 @@
+build/default/production/HW_5_I2C.o:  \
+HW_5_I2C.c 
